1701a6cb363c521f352186e9c268b30dcf8e452a
juancho20sp <juancho20sp@gmail.com>
2020-06-04 18:50:59 -0500

    Router created

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <main>
+    <px-header></px-header>
+    <router-view></router-view>
+  </main>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import PxHeader from "@/components/PxHeader";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    PxHeader
   }
 };
 </script>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <px-assets-table></px-assets-table>
+  </div>
+</template>
+
+<script>
+import PxAssetsTable from "@/components/PxAssetsTable";
+
+export default {
+  name: "Home",
+
+  components: {
+    PxAssetsTable
+  }
+};
+</script>
